f7372cadebfbd19d5b1780243e56b30b1e073fdc
EmpaAIonSol <empasol@tuta.io>
2025-02-21 14:05:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Dodatkowa logika JavaScript (np. obsługa scrollowania)
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll("section");
+
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("visible");
+                }
+            });
+        },
+        { threshold: 0.1 }
+    );
+
+    sections.forEach((section) => {
+        observer.observe(section);
+    });
+});
